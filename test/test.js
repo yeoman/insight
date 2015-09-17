9e4bb9c4a54c5a5a7f499f5e693f7d40694eac4f
Michael Storgaard <git@mex.io>
2015-09-17 15:46:05 +0200

    Added optional permission timeout

--- test/test.js
+++ test/test.js
@@ -154,4 +154,17 @@ describe('askPermission', function () {
 			done();
 		});
 	});
+
+	it('should skip after timeout', function (done) {
+		var env = JSON.parse(JSON.stringify(process.env));
+		env.permissionTimeout = 0.1;
+
+		var insProcess = spawn('node', [
+			'./test/fixtures/sub-process.js'
+		], {stdio: 'inherit', env: env});
+		insProcess.on('close', function (code) {
+			assert.equal(code, 145);
+			done();
+		});
+	});
 });
