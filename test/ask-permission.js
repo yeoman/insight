cb32c1a043c54d50ef1aff04c7a70f761bcb8035
Raphael von der Grün <raphinesse@gmail.com>
2019-04-11 16:39:48 +0200

    Update xo & ava to fix tests (#63)
    On a fresh install, the tests are currently failing because of breaking changes in `ava`. This PR updates the tests to work with `ava@1` and also updates `xo` so that new `ava` API is correctly recognized.

--- test/ask-permission.js
+++ test/ask-permission.js
@@ -2,33 +2,33 @@ import execa from 'execa';
 import test from 'ava';
 
 test('skip in TTY mode', async t => {
-	const err = await t.throws(execa('node', ['./test/fixtures/sub-process.js']));
+	const err = await t.throwsAsync(execa('node', ['./test/fixtures/sub-process.js']));
 	t.is(err.code, 145);
 });
 
 test('skip when using the --no-insight flag', async t => {
-	const err = await t.throws(execa('node', ['./test/fixtures/sub-process.js', '--no-insight'], {stdio: 'inherit'}));
+	const err = await t.throwsAsync(execa('node', ['./test/fixtures/sub-process.js', '--no-insight'], {stdio: 'inherit'}));
 	t.is(err.code, 145);
 });
 
 test('skip in CI mode', async t => {
-	const CI = process.env.CI;
+	const {CI} = process.env;
 	process.env.CI = true;
 
-	const err = await t.throws(execa('node', ['./test/fixtures/sub-process.js'], {stdio: 'inherit'}));
+	const err = await t.throwsAsync(execa('node', ['./test/fixtures/sub-process.js'], {stdio: 'inherit'}));
 	t.is(err.code, 145);
 
 	process.env.CI = CI;
 });
 
 test('skip after timeout', async t => {
-	const CI = process.env.CI;
-	const permissionTimeout = process.env.permissionTimeout;
+	const {CI} = process.env;
+	const {permissionTimeout} = process.env;
 
 	process.env.CI = true;
 	process.env.permissionTimeout = 0.1;
 
-	const err = await t.throws(execa('node', ['./test/fixtures/sub-process.js'], {stdio: 'inherit'}));
+	const err = await t.throwsAsync(execa('node', ['./test/fixtures/sub-process.js'], {stdio: 'inherit'}));
 	t.is(err.code, 145);
 
 	process.env.CI = CI;
