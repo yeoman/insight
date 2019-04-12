b6a34d53b6c19589e6e3cb1a7a0751f3ec5d2c16
Raphael von der Grün <raphinesse@gmail.com>
2019-04-12 12:23:36 +0200

    Revert "Make debouncing tests reliable (#65)"
    This reverts commit 10f497da942ba4d46173c2bae7039b0a4be3b3ef.

--- test/insight.js
+++ test/insight.js
@@ -1,3 +1,4 @@
+import objectValues from 'object-values';
 import sinon from 'sinon';
 import test from 'ava';
 import Insight from '../lib';
@@ -18,80 +19,92 @@ test('throw exception when trackingCode or packageName is not provided', t => {
 	/* eslint-enable no-new */
 });
 
-test.cb('sends right after calling track()', t => {
+test.cb('forks a new tracker right after track()', t => {
 	const insight = newInsight();
 	insight.track('test');
 	setImmediate(() => {
-		t.is(insight._send.callCount, 1);
+		t.deepEqual(forkedCalls(insight), [
+			// A single fork with a single path
+			['/test']
+		]);
 		t.end();
 	});
 });
 
-test.cb('only sends once if many pages are tracked in the same event loop run', t => {
+test.cb('only forks once if many pages are tracked in the same event loop run', t => {
 	const insight = newInsight();
 	insight.track('foo');
 	insight.track('bar');
 	setImmediate(() => {
-		t.is(insight._send.callCount, 1);
+		t.deepEqual(forkedCalls(insight), [
+			// A single fork with both paths
+			['/foo', '/bar']
+		]);
 		t.end();
 	});
 });
 
-test.cb('debounces sending every 100 millis (close together)', t => {
+test.cb('debounces forking every 100 millis (close together)', t => {
 	const insight = newInsight();
-
-	// The first one is sent straight away because of the leading debounce
 	setTimeout(() => insight.track('0'), 0);
-
-	// The others are grouped together because they're all < 100ms apart
 	setTimeout(() => insight.track('50'), 50);
 	setTimeout(() => insight.track('100'), 100);
 	setTimeout(() => insight.track('150'), 150);
 	setTimeout(() => insight.track('200'), 200);
-
 	setTimeout(() => {
-		t.is(insight._send.callCount, 2);
+		t.deepEqual(forkedCalls(insight), [
+			// The first one is sent straight away because of the leading debounce
+			['/0'],
+			// The others are grouped together because they're all < 100ms apart
+			['/50', '/100', '/150', '/200']
+		]);
 		t.end();
 	}, 1000);
 });
 
-test.cb('debounces sending every 100 millis (far apart)', t => {
+test.cb('debounces forking every 100 millis (far apart)', t => {
 	const insight = newInsight();
-
-	// Leading call
 	setTimeout(() => insight.track('0'), 0);
-
-	// Sent together since there is an empty 100ms window afterwards
 	setTimeout(() => insight.track('50'), 50);
 	setTimeout(() => insight.track('100'), 100);
 	setTimeout(() => insight.track('150'), 150);
-
-	// Sent on its own because it's a new leading debounce
 	setTimeout(() => insight.track('300'), 300);
-
-	// Finally, the last one is sent
 	setTimeout(() => insight.track('350'), 350);
-
 	setTimeout(() => {
-		t.is(insight._send.callCount, 4);
+		t.deepEqual(forkedCalls(insight), [
+			// Leading call
+			['/0'],
+			// Sent together since there is an empty 100ms window afterwards
+			['/50', '/100', '/150'],
+			// Sent on its own because it's a new leading debounce
+			['/300'],
+			// Finally, the last one is sent
+			['/350']
+		]);
 		t.end();
 	}, 1000);
 });
 
 // Return a valid insight instance which doesn't actually send analytics (mocked)
 function newInsight() {
-	// _debouncedSend wraps _send during construction, so we have to replace
-	// Insight.prototype._send with a stub. However, if we do that on Insight,
-	// then tests executed in parallel will share the same stub. That's why we
-	// create a new class for every caller.
-	class StubbedInsight extends Insight {}
-	StubbedInsight.prototype._send = sinon.stub();
-
-	const insight = new StubbedInsight({
+	const insight = new Insight({
 		trackingCode: 'xxx',
 		packageName: 'yeoman',
 		packageVersion: '0.0.0'
 	});
 	insight.optOut = false;
+	insight._fork = sinon.stub();
 	return insight;
+}
+
+// Returns all forked calls, and which paths were tracked in that fork
+// This is handy to get a view of all forks at once instead of debugging 1 by 1
+// [
+//   ['/one', 'two'],       // first call tracked 2 paths
+//   ['/three', 'four'],    // second call tracked 2 more paths
+// ]
+function forkedCalls(insight) {
+	return insight._fork.args.map(callArgs => {
+		return objectValues(callArgs[0].queue).map(q => q.path);
+	});
 }
